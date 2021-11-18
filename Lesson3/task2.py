043b36e60a57ba590c4381f607f0a422b582328c
serzh251 <serzh251@mail.ru>
2021-11-18 17:25:17 +0300

    Lesson3 done

--- Lesson3/task2.py
+++ Lesson3/task2.py
@@ -1,8 +1,13 @@
+"""Написать программу, которая запрашивает у пользователя ввод числа.
+На введенное число она отвечает сообщением, целое оно или дробное.
+Если дробное — необходимо далее выполнить сравнение чисел до и после запятой.
+Если они совпадают, программа должна возвращать значение True, иначе False."""
+
 import math
 
 
 def check_number():
-    number = 0.0
+    """без обработки ', ' """
     try:
         number = float(input('введите число: '))
 
@@ -15,14 +20,40 @@ def check_number():
         number_str = str(number)
         number_list = number_str.split('.')
         if number_list[0] == number_list[1]:
+            print("число до запятой равно числу после запятой")
             return True
         else:
+            print("число до запятой и после нее не равны")
             return False
     else:
         print("введенное число целое")
+        return
 
 
-if check_number():
-    print("число до запятой равно числу после запятой")
-else:
-    print("число до запятой и после нее не равны")
+def check_number_2():
+    """обработка с ',' и '.' """
+    number = input('введите число: ')
+
+    if len(number.split('.')) > 1:
+        number_lst = number.split('.')
+    else:
+        number_lst = number.split(',')
+
+    if len(number_lst) > 1:
+        print('введенное число дробное')
+        if number_lst[0] == number_lst[1]:
+            print("число до запятой равно числу после запятой")
+            return True
+        else:
+            print("число до запятой и после нее не равны")
+            return False
+    try:
+        number = int(number)
+        print('введенное число целое')
+    except:
+        print('вы ввели не число')
+
+
+check_number()
+
+check_number_2()
--- Lesson3/task3.py
+++ Lesson3/task3.py
@@ -1,15 +1,20 @@
+"""Создать два списка с различным количеством элементов. В первом должны быть записаны ключи, во втором — значения.
+Необходимо написать функцию, создающую из данных ключей и значений словарь.
+Если ключу не хватает значения, в словаре для него должно сохраняться значение None.
+Значения, которым не хватило ключей, необходимо отбросить."""
+
 
 keys = [_ for _ in range(1, 10)]
 values = ['first', 'second', 'third', 'fourth', 'fifth']
-my_dict = {}
 
-# my_dict.update([el])
-# my_dict = {key: [list2] for key in zip(list1)}
-# my_dict = dict(zip(list2, list1))
-# my_dict.fromkeys(list1[ zip(list2)])
-# for value in list2:
-#     for key in list1:
-#         my_dict.fromkeys(key[value])
-# my_dict = my_dict.fromkeys(list1)
-my_dict = {key: value for key, value in zip(keys, values)}
+
+my_dict1 = {key: value for key, value in zip(keys, values)}
 print(my_dict)
+
+my_dict2 = {}
+for i in keys:
+    try:
+        my_dict2[i] = values[i]
+    except:
+        my_dict2[i] = None
+print(my_dict2)
